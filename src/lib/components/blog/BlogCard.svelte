4e453fd1bbecbc721ccd58f1e51a49e257bd65ca
E Lefkoski <lefkoski240@gmail.com>
2024-11-07 20:47:41 -0500

    Finished Blog Home page but not schema

--- src/lib/components/blog/BlogCard.svelte
+++ src/lib/components/blog/BlogCard.svelte
@@ -3,16 +3,26 @@
 		title: string;
 		description: string;
 		href: string;
-		imageUrl: string;
+		imageUrlSmall: string;
+		imageUrlMedium: string;
+		imageUrlLarge: string;
 		imageAlt: string;
 	};
 </script>
 
-<div class="card bg-white border border-gray-200 rounded-lg p-4 shadow-sm flex flex-col h-full">
-	<img src={blog.imageUrl} alt={blog.imageAlt} class="w-full h-48 object-cover rounded-t-lg" />
-	<div class="p-4 flex flex-col flex-grow">
+<div
+	class="card border blog-home-page-section-border rounded-lg shadow-sm flex flex-col h-full home-page-body-bg-1"
+>
+	<img
+		srcset="{blog.imageUrlSmall} 480w, {blog.imageUrlMedium} 768w, {blog.imageUrlLarge} 1024w"
+		sizes="(max-width: 480px) 480px, (max-width: 768px) 768px, 1024px"
+		src={blog.imageUrlLarge}
+		alt={blog.imageAlt}
+		class="w-full h-48 object-cover rounded-t-lg"
+	/>
+	<div class="p-8 flex flex-col flex-grow">
 		<h2 class="text-xl font-bold">{blog.title}</h2>
 		<p class="flex-grow">{blog.description}</p>
-		<a href={blog.href} class="text-blue-500 hover:underline">Read more</a>
+		<a href={blog.href} class="hyperlink hover:underline">Read more</a>
 	</div>
 </div>
